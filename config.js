4cae9598f80261bb761ccc6abbddbebe1fbd14dd
diegoquirama <jberkhan@yahoo.com>
2022-12-22 14:00:22 -0500

    Primer Commit del Backend

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const port_db = process.env.PORT_DB
 const user_db = process.env.USER.DB
 const password_db = process.env.PASSWORD.DB
 const name_db = process.env.NAME_DB
-const host.db = process.env.HOST_DB
+const host_db = process.env.HOST_DB
 
 module.exports = {
     port_server,
